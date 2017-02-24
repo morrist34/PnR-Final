2fcb0d98f7e1bbf73e23d31e6940e421184ec410
morrist <tuckermorris00@gmail.com>
2017-02-24 11:21:52 -0500

    return starting position

--- student.py
+++ student.py
@@ -48,7 +48,7 @@ class GoPiggy(pigo.Pigo):
         menu = {"n": ("Navigate forward", self.nav),
                 "d": ("Dance", self.dance),
                 "c": ("Calibrate", self.calibrate),
-                "w": ("Sweep", self.sweep),
+                "t": ("Turn Test", self.turn_test),
                 "s": ("Check status", self.status),
                 "q": ("Quit", quit)
                 }
@@ -59,6 +59,26 @@ class GoPiggy(pigo.Pigo):
         ans = raw_input("Your selection: ")
         # activate the item selected
         menu.get(ans, [None, error])[1]()
+
+    def turn_test(self):
+        while True:
+            ans = raw_input('Turn right, left or stop? (r/l/s): ')
+            if ans == 'r':
+                val = int(raw_input('/nBy how much?: '))
+                self.encR(val)
+            elif ans == 'l':
+                val = int(raw_input('/nBy how much?: '))
+                self.encL(val)
+            else:
+                break
+        self.restore_heading()
+
+    def restore_heading(self):
+        print("Now I'll turn back to the starting position.")
+        if self.turn_track > 0:
+            self.encL(abs(self.turn_track))
+        elif self.turn_track < 0:
+            self.encR(abs(self.turn_track))
 
     def sweep(self):
         for x in range(self.MIDPOINT - 60, self.MIDPOINT + 60, 2):
@@ -86,7 +106,7 @@ class GoPiggy(pigo.Pigo):
         self.sprinkler()
 
     def shimmy(self):
-        print ('shimmy')
+        print('shimmy')
         for y in range(3):
             for x in range(150,250,50):
                 self.set_speed(x,x)
@@ -100,7 +120,7 @@ class GoPiggy(pigo.Pigo):
             self.encL(5)
 
     def chacha(self):
-        print ('chacha')
+        print('chacha')
         for x in range(2):
             self.servo(0)
             self.servo(45)
@@ -127,7 +147,7 @@ class GoPiggy(pigo.Pigo):
             self.servo(30)
 
     def sprinkler(self):
-        print ('sprinkler')
+        print('sprinkler')
         for x in range(160,20,15):
             self.servo(x)
 
@@ -143,7 +163,13 @@ class GoPiggy(pigo.Pigo):
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         # this is the loop part of the "main logic loop"
 
+    def encR(self, enc):
+        pigo.Pigo.encR(self, enc)
+        self.turn_track += enc
 
+    def encL(self, enc):
+        pigo.Pigo.encL(self, enc)
+        self.turn_track -= enc
 
 
 ####################################################
