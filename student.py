3d170c3b4b339c25a6f9890dfc950f7252211987
morrist <tuckermorris00@gmail.com>
2017-04-19 11:32:20 -0400

    deleted wide scan from final

--- student.py
+++ student.py
@@ -215,7 +215,6 @@ class GoPiggy(pigo.Pigo):
         while True:
             if self.is_clear():
                 self.cruise()
-            self.wide_scan()
             answer = self.choose_path()
             if answer == "left":
                 self.encL(4)
